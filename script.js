09090c36d024d06600e7c28922956d5f5f03aea8
azizbek1701 <azizbekkamilov1701@gmail.com>
2025-06-24 14:16:41 +0500

    website is done

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 let currentIndex = 0;
-  const slidesContainer = document.getElementById('carouselSlides');
-  const totalSlides = document.querySelectorAll('.carousel-slide').length;
+const slidesContainer = document.getElementById('carouselSlides');
+const totalSlides = document.querySelectorAll('.carousel-slide').length;
 
-  function showSlide(index) {
-    if (index >= totalSlides) currentIndex = 0;
-    else if (index < 0) currentIndex = totalSlides - 1;
-    else currentIndex = index;
+function showSlide(index) {
+  if (index >= totalSlides) currentIndex = 0;
+  else if (index < 0) currentIndex = totalSlides - 1;
+  else currentIndex = index;
 
-    slidesContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
-  }
+  slidesContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
 
-  function nextSlide() {
-    showSlide(currentIndex + 1);
-  }
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
 
-  function prevSlide() {
-    showSlide(currentIndex - 1);
-  }
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
 
-  setInterval(nextSlide, 3000); // Auto-slide every 6 seconds
+setInterval(nextSlide, 3000); // Auto-slide every 6 seconds
 
 
 
